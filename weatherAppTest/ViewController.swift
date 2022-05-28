0354f41e67e33e4eadffce8653e905dee07245ae
sem1tevrizoff <pasqoooda@gmail.com>
2022-05-28 20:49:31 +0300

    WIP

--- weatherAppTest/ViewController.swift
+++ weatherAppTest/ViewController.swift
@@ -7,7 +7,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getSomeThing(choose: "")
+        getSomeThing(choose: "Brest")
         
     }
     
