93c37f00ce861454a00f059dd986575d6f128ca7
sem1tevrizoff <pasqoooda@gmail.com>
2022-06-04 02:35:19 +0300

    WIP

--- weatherAppTest/MainViewController.swift
+++ weatherAppTest/MainViewController.swift
@@ -21,6 +21,13 @@ class MainViewController: UIViewController {
     init(presenter: MainPresenter) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
+        
+        
+        
+        
+        
+        
+        
     }
     
     required init?(coder: NSCoder) {
