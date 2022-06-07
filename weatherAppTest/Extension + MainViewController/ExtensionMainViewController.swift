138b0218cceed977005e02d81ef2f3391837a5e7
Vadim Shalugin <vd.shalugin@gmail.com>
2022-06-07 19:45:07 +0400

    Add comments

--- weatherAppTest/Extension + MainViewController/ExtensionMainViewController.swift
+++ weatherAppTest/Extension + MainViewController/ExtensionMainViewController.swift
@@ -1,5 +1,7 @@
 import Foundation
 import UIKit
+
+#warning("Делать extension отдельным файлом, если это extension одного вьюконтроллера, фиговая практика. Лучше их деражать в одном классе с контроллером. А в extension полноценные extension - как твой Float")
 extension MainViewController: MainViewDelegate {
     
     func updateUI(with weather: [Weather]) {
@@ -39,3 +41,40 @@ extension MainViewController {
         self.present(alertVC, animated: true)
     }
 }
+
+#warning("Если очень хочется extension можно сдедать как внизу. Но можно и просто перенести функцию showAlert() в MainViewController")
+
+/*
+extension UIViewController {
+    
+    func showCityAlert(with completion: @escaping (String) -> Void) {
+        let alertVC = UIAlertController(title: "Choose City",
+                                        message: nil,
+                                        preferredStyle: .alert)
+        alertVC.addTextField { textField in
+            textField.placeholder = "City Name"
+        }
+        
+        let allertAction = UIAlertAction(title: "Find", style: .default) { UIAlertAction in
+            let firstTextField = alertVC.textFields![0] as UITextField
+            guard let cityName = firstTextField.text else { return }
+            completion(cityName)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alertVC.addAction(allertAction)
+        alertVC.addAction(cancelAction)
+        
+        self.present(alertVC, animated: true)
+    }
+}
+
+//Вызывать так из ViewController'а
+
+showCityAlert { [weak self] cityName in
+    self?.presenter.setUpMainInfoLabels(choose: cityName)
+}
+ 
+//Еще вариант - сделать класс типа AlertPresenter 
+*/
--- weatherAppTest/Extension + MainViewController/ProtocolMainViewDelegate.swift
+++ weatherAppTest/Extension + MainViewController/ProtocolMainViewDelegate.swift
@@ -3,4 +3,27 @@ import Foundation
 protocol MainViewDelegate: AnyObject {
     func updateUI(with weather: [Weather])
     func setUpMainLabel(city: String, temp: Float, descriptionWeather: String, maxTemp: Float, minTemp: Float)
+    #warning("Много параметров в функции не есть хорошо. Я бы сделал модель MainViewModel и собирал бы её в презентере. Норм практика. Пример внизу")
 }
+
+/*
+struct MainViewModel {
+    let city: String
+    let temp: Float
+    let descriptionWeather: String
+    let maxTemp: Float
+    let minTemp: Float
+}
+
+//-> В презентере
+let model = MainViewModel(
+    city: weatherModel.name,
+    temp: weatherModel.main.temp,
+    descriptionWeather: weatherModel.weather[0].description,
+    maxTemp: weatherModel.main.tempMax,
+    minTemp: weatherModel.main.tempMin
+)
+
+self.viewDelegate?.setUpMainLabel(with: model)
+
+*/
